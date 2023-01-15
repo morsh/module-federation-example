9cb583faf65d16da62321745b49ce29c9abba90a
Mor Shemesh <mor.shemesh@gmail.com>
2023-01-15 21:24:22 +0200

    raise coverage to 100%

--- remote-component/src/redux/reducers/simpleSlice.ts
+++ remote-component/src/redux/reducers/simpleSlice.ts
@@ -5,7 +5,5 @@ export const simpleSlice = <T>(name: string, initialState: T) =>
     name,
     initialState,
     reducers: {
-      set: (_, { payload }: { type: string; payload: T }) => payload,
-      update: (state, action: { type: string; payload: Partial<T> }) => ({ ...state, ...action.payload }),
     },
   });
